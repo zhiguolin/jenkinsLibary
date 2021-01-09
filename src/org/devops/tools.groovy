6252b1ac35afeca6b7504598f7aaa9e8e102e303
zhiguolin <13611296562@163.com>
2021-01-09 16:32:52 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,6 +1,12 @@
 package org.devops
 
 //打印
-def PringMsg(content){
-    println(content)
+def PrintMsg(value,color){
+	colors = [
+		'red' : "\033[40;31m>>>>>>>>>${value}<<<<<<<<<<<\033[0m",
+		'blue' : "\033[47;34m>>>>>>>>>${value}<<<<<<<<<<<\033[0m",
+		'green1' : "\033[40;32m>>>>>>>>>${value}<<<<<<<<<<<\033[0m"
+	]
+	ansiColor('xterm')
+	println(colors[color])
 }
